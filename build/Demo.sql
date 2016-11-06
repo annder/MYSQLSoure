f4641e218a79b519c962a8f493073812eedaf4e5
annder <2461067798@qq.com>
2016-11-06 19:33:55 +0800

    Create Demo.sql

---
+++ build/Demo.sql
@@ -0,0 +1,8 @@
+create database student;
+use student;
+create table information(
+    idName int,
+    Name varchar(20)
+);
+alter table student add coumn sg varchar(1024);
+alter table student change javascript vuejavascript varchar(1024);
